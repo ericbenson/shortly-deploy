f13692d2a27945157e1dd43bcc95c3c26c948d88
Hack Reactor Students <accounts@hackreactor.com>
2015-01-14 21:21:30 -0800

    Set up mongod

--- app/config.js
+++ app/config.js
@@ -1,45 +1,18 @@
-var Bookshelf = require('bookshelf');
-var path = require('path');
+var mongoose = require('mongoose');
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
+var db = mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+  // Create your schemas and models here.
+  var movieSchema = new mongoose.Schema({
+    title: { type: String },
+    rating: String,
+    releaseYear: Number,
+  , hasCreditCookie: Boolean
+  });
 });
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+mongoose.connect('mongodb://localhost/test');
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
-
-module.exports = db;
+//mongodb://MongoLab-r:t2GV1vSlVuEEr2sUirN4.3W2d2OEz2bDQFnYDegGv6g-@ds052837.mongolab.com:52837/MongoLab-r
